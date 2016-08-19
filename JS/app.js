e178000dd6d576fb34b8d3ec0c032bbd3b93ae7b
mykeels <mykehell123@gmail.com>
2016-08-19 13:40:22 +0100

    Push all Files
    First Commit ... Done with this for now ... It works perfectly

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("EightQueensApp", []);;
+
+document.addEventListener("general-scripts-loaded", function () {
+    app = angular.module("WakanowPayment", ['objectTable', 'angular-json-tree', 'ajs.RecursiveDirectiveHelper']);
+});
+
+/*app.config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'http://www.refsnesdata.no/**'
+    ]);
+});*/
